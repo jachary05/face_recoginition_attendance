6f86647fc965dc75caa54f54481cf07cacabdc65
jachary05 <155758949+jachary05@users.noreply.github.com>
2024-04-20 20:43:58 +0530

    files added

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import time 
+from datetime import datetime
+
+ts=time.time()
+date=datetime.fromtimestamp(ts).strftime("%d-%m-%Y")
+timestamp=datetime.fromtimestamp(ts).strftime("%H:%M-%S")
+
+from streamlit_autorefresh import st_autorefresh
+# Run the autorefresh about every 2000 milliseconds (2 seconds) and stop
+# after it's been refreshed 100 times.
+count = st_autorefresh(interval=2000, limit=100, key="fizzbuzzcounter")
+# The function returns a counter for number of refreshes. This allows the
+# ability to make special requests at different intervals based on the count
+if count == 0:
+    st.write("Count is zero")
+elif count % 3 == 0 and count % 5 == 0:
+    st.write("FizzBuzz")
+elif count % 3 == 0:
+    st.write("Fizz")
+elif count % 5 == 0:
+    st.write("Buzz")
+else:
+    st.write(f"Count: {count}")
+
+
+df=pd.read_csv("Attendance/Attendance_" + date + ".csv")
+
+st.dataframe(df.style.highlight_max(axis=0))
